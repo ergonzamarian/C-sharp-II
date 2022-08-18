04496f94c313efc71f840b90f2682de37c3923aa
= <=>
2022-08-18 00:39:50 -0400

    Adicionando Projeto inicial com funções de saque, depósito e transferência bancária

---
+++ bytebank/bytebank/ContaCorrente.cs
@@ -0,0 +1,48 @@
+﻿namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numero_agencia;
+        public string nome_agencia;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true; 
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo = saldo + valor;
+            }
+            
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor || saldo < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+
+                return true;
+            }
+        }
+    }
+}
---
+++ bytebank/bytebank/Program.cs
@@ -0,0 +1,33 @@
+﻿using bytebank;
+
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Ergon";
+conta1.conta = "12345-x";
+conta1.numero_agencia = 23;
+conta1.nome_agencia = "Agência Central";
+conta1.saldo = 80;
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "André";
+conta2.conta = "12546-x";
+conta2.numero_agencia = 23;
+conta2.nome_agencia = "Agência Central";
+conta2.saldo = 150;
+
+//Console.WriteLine("Titular: " + conta1.titular);
+//Console.WriteLine("Conta: " + conta1.conta);
+//Console.WriteLine("Número Agência: " + conta1.numero_agencia);
+//Console.WriteLine("Nome Agência: " + conta1.nome_agencia);
+//Console.WriteLine("Saldo: " + conta1.saldo);
+
+Console.WriteLine("Saldo do Ergon: " + conta1.saldo);
+Console.WriteLine("Saldo do André: " + conta2.saldo);
+
+conta1.Transferir(50, conta2);
+
+Console.WriteLine("Saldo do Ergon: " + conta1.saldo);
+Console.WriteLine("Saldo do André: " + conta2.saldo);
+
+Console.ReadKey();
